935aa5c76f6bc240f8e83c50780650ea5289213b
Jean-François Dostaler <jf.dostaler@gmail.com>
2015-04-26 13:23:14 -0400

    Check in R script

---
+++ run_analysis.R
@@ -0,0 +1,84 @@
+
+
+# Save original working directory
+origwd <- getwd()
+
+
+# Install and load required packages if necessary
+loadPackage <- function (pkg) {
+  if (! pkg %in% installed.packages()) {
+    message("Installing package: " , pkg)
+    install.packages( toString(pkg) )
+  }
+  require( pkg, character.only = TRUE )
+}
+loadPackage('data.table')
+loadPackage('dplyr')
+
+
+# Download the dataset if it's not in the working directory
+URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+DESTFILE <- "dataset.zip"
+
+if (!DESTFILE %in% dir()) {
+  message("Downloading data from: " , URL)
+  download.file(url = URL, destfile = "dataset.zip", method = "auto", mode = "wb")
+} else {
+  message("The dataset archive is already present in the working directory.")
+}
+
+
+# Extract the data from the .zip file
+message("Extracting data from archive.")
+unzip(DESTFILE)
+setwd("UCI HAR Dataset")
+
+# Step 1: Merge the test and training data
+
+# Load in the data
+message('Reading test data.')
+testdata <- cbind(
+  read.table('test/subject_test.txt'),
+  read.table('test/y_test.txt'),
+  read.table('test/X_test.txt')
+)
+
+message('Reading training data.')
+traindata <- cbind(
+  read.table('train/subject_train.txt'),
+  read.table('train/y_train.txt'),
+  read.table('train/X_train.txt')
+)
+
+message('Combining data sets.')
+dt <- data.table(rbind(testdata, traindata))
+
+# set column names from features.txt
+message('Setting column names')
+colnames <- fread('features.txt')$V2
+setnames(dt, c("Subject", "Activity", colnames))
+
+activitynames <- fread('activity_labels.txt')$V2
+
+#2. Extract only columns that contain the mean and standard deviation
+setkey(dt, Subject, Activity)
+#dt <- dt[, c(1:2, grep("-(std|mean)\\(\\)", names(dt))), with=FALSE]
+tidyData <- dt %>%
+  select(Subject, Activity, matches("-(std|mean)\\(\\)")) %>%
+  mutate(Activity = activitynames[Activity]) %>%
+  group_by(Subject, Activity)
+
+#4. Give descriptive column names
+
+#5. Aggregate by activity/subject
+message('Calculating means')
+summaryData <- tidyData %>% summarise_each(funs(mean))
+
+# Clean up
+setwd(origwd)
+write.table(tidyData, file = "full_data.txt", row.names = FALSE)
+write.table(summaryData, file = "summarized_data.txt", row.names = FALSE)
+
+message('Done!')
+message('Step #4 Tidy data is in variable "tidyData"')
+message('Step #5 Summarized data is in variable "summaryData"')
